b4bd5af4359ed7901bf5ad7b5998d1ccab5f545a
miguelr215 <drexelramos@gmail.com>
2025-08-30 06:48:51 -0400

    component reorg

--- app/components/AddToCart.tsx
+++ app/components/AddToCart.tsx
@@ -1,9 +1,10 @@
 'use client';
 import React from 'react'
+import styles from './ProductCard/ProductCard.module.css'
 
 const AddToCart = () => {
   return (
-    <div>
+    <div className={styles.card}>
         <button onClick={() => console.log('clicked')}>Add to Cart</button>
     </div>
   )
